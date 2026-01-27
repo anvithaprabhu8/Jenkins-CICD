da9a08e74ac3eb6ed6a1a8686e2f741379fdc678
anvithaprabhu8 <39764931+anvithaprabhu8@users.noreply.github.com>
2026-01-27 17:41:17 +0530

    Fix syntax in test.sh for output checking

--- shell-script/test.sh
+++ shell-script/test.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 output=$(./app.sh)
 
-if [[ $output == *"Hello"* ]]; then
-echo "Test has passed"
-exit 0
+if [[ "$output" == *"Hello"* ]]; then
+   echo "Test has passed"
+   exit 0
 else
-echo "Test has failed"
-exit 1
+   echo "Test has failed"
+   exit 1
 fi
